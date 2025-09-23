c2155820f2dec91ac4fbea45a366281f92b1cd96
Abhishek Yadav <timepass80100@gmail.com>
2025-09-23 13:41:30 +0530

    script.js

--- script.js
+++ script.js
@@ -475,8 +475,8 @@ function addMessageToChat(message, sender) {
   <span> 
 
     <svg 
-    width="16" 
-    height="16" 
+    width="20" 
+    height="20" 
     viewBox="0 0 16 16" 
     fill="currentColor" 
     xmlns="http://www.w3.org/2000/svg">
